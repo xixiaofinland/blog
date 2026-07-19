1b6e9cf9a87836b61fd0d5554232f1e4eaa2b1d5
xixiaofinland <xixiaofinland@users.noreply.github.com>
2026-07-19 18:21:17 +0000

    deploy: f016f2502b932977043ddd3751f8a67aebc969c8

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-390324a6.js',
+                path_to_root + 'searchindex-f6a5a647.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
